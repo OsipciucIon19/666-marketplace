bd1ce69e08f918c43ab56b29753953f9b5aa0e46
OsipciucIon19 <ion-osipciuc@mail.ru>
2021-12-01 19:16:15 +0200

    SJ666-16-add styles to main page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import photo from "./data/sample_img.jpg"
 function App() {
     return (
         <div className="App">
-            666 - marketplace
-            <h1>feature</h1>
+            <header>666 - marketplace</header>
             <div className="mainContent">
                 {
                     data.default.map((ad) =>
-                        <div key={ad.id}>
-                            <h1>{ad.title}</h1>
-                            <img src={photo} alt="ad-img" style={{"width": 100}}/>
-                            <div>{ad.price} $</div>
-                            <div>{ad.description}</div>
-                        </div>
+                            <div className="card" key={ad.id}>
+                                <h3 className="title">{ad.title}</h3>
+                                <img src={photo} className="image" alt="ad-img"/>
+                                <div className="price">{ad.price} $</div>
+                                <div className="description">{ad.description}</div>
+                            </div>
                     )
-                }
-            </div>
+                }</div>
         </div>
     );
 }
