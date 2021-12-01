793ca1f25474fd8827ab4dffd3162bd575195f63
OsipciucIon19 <ion-osipciuc@mail.ru>
2021-12-01 15:43:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import * as data from "./data/data.json"
+import photo from "./data/sample_img.jpg"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            666 - marketplace
+            <div className="mainContent">
+                {
+                    data.default.map((ad) =>
+                        <div key={ad.id}>
+                            <h1>{ad.title}</h1>
+                            <img src={photo} alt="ad-img" style={{"width": 100}}/>
+                            <div>{ad.price} $</div>
+                            <div>{ad.description}</div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
